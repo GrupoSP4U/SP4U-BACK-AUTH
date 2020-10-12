0b50e9faba87c2bca684cd2abca0786484ef6d85
Victor Massao <victor.massao@cerc.inf.br>
2020-10-12 20:13:18 -0300

    Arruma o error code da autenticação
    - Quando a requisição era feita com a senha incorreta, estava retornando BAD_REQUEST mas deveria ser retornado NOT_FOUND.
    - Adicionei os validadores @NotNull e @Valid para retornar BAD_REQUEST quando receber uma requisição de token com os campos nulos.

--- src/main/java/com/bandtec/application/requests/AuthenticationRequest.java
+++ src/main/java/com/bandtec/application/requests/AuthenticationRequest.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Getter
@@ -15,6 +16,8 @@ public class AuthenticationRequest implements Serializable {
 
     private static final long serialVersionUID = 5926468583005150707L;
 
+    @NotNull
     private String email;
+    @NotNull
     private String password;
 }
